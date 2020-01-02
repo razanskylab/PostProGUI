962b296207fe0201359dcab2a7c04619ffecaceb
SteffiDesktop <johannesrebling@gmail.com>
2020-01-02 16:58:40 +0100

    added matlab startup file

---
+++ startup.m
@@ -0,0 +1,10 @@
+%%==============================================================================
+addpath(genpath(pwd));
+
+rmpath('.git')
+
+set(0,'DefaultAxesFontSize',12);
+set(0,'DefaultTextFontSize',12);
+set(0,'DefaultLineLinewidth',1.5);
+format compact;
+set(0,'defaultfigurecolor',[1 1 1]);
