de87c1cbb11ca52ee1a163a1ff183972b15f79a1
Johannes Rebling <johannesrebling@gmail.com>
2020-04-02 11:22:18 +0200

    added easy startup, cleaned up folder structure

--- StartGui.m
+++ StartGui.m
@@ -1,12 +1,18 @@
-%%==============================================================================
+% make sure we have all folders and subfolders added to matlab path
 addpath(genpath(pwd));
 
+% remove git folder if one exists...
 if isfolder('.git')
   rmpath('.git');
 end
 
+% get default figure background to be white, have decent default fontsize
+% as well
 set(0,'DefaultAxesFontSize',12);
 set(0,'DefaultTextFontSize',12);
 set(0,'DefaultLineLinewidth',1.5);
 format compact;
 set(0,'defaultfigurecolor',[1 1 1]);
+
+% start actual GUI
+MasterGui();
